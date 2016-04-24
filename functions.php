6955d1563112ddc5b374356dfa0e5428de38657b
emhr <eugene.marseilles.hornot.robinson@gmail.com>
2016-04-24 17:27:59 -0400

    Functions.php: Changes to namespacing and imported file structure
    Backward compatibility changed to WP 4.5.
    
    A new more condensed structure to required files. All the action hooks
    getting stored in their own file actions.php. this cuts down on a lot
    of clutter in the other files. The discussion-extensions and
    comment-extensions.php are getting consolidated to
    comment-extensions.php. The widgets-extensions.php is going away.
    Deciduous will not be creating custom widgets or presetting widget
    areas with widgets upon activation like Thematic did.
    
    Deciduous will also not be removing the WordPress generator from the
    head. Child themes can if they like.
    
    New WP theme support additions 'title-tag' 'html5' 'custom-header'
    
    Deciduous will start prefixing it's own theme support tags with
    deciduous_s_
    
    Plugable functions are no longer prefixed with childtheme_override_
    instead they will be prefixed with deciduous_p_

--- functions.php
+++ functions.php
@@ -3,204 +3,161 @@
  * Theme Functions
  *
  * This file is used by WordPress to initialize the theme.
- * Thematic is designed to be used as a theme framework and this file should not be modified.
+ * Deciduous is designed to be used as a theme framework and this file should not be modified.
  * You should use a Child Theme to make your customizations. A sample child theme is provided
  * as an example in root directory of this theme. You can move it into the wp-content/themes to
  * enable activation of the child theme. <br>
  *
- * Reference:  {@link http://codex.wordpress.org/Child_Themes Codex: Child Themes}
+ * @link http://codex.wordpress.org/Child_Themes Codex: Child Themes
  * 
- * @package Thematic
+ * @package Deciduous
  * @subpackage ThemeInit
  */
 
 
 /**
- * Thematic only works in WordPress 3.6 or later.
+ * Deciduous only works in WordPress 4.4 or later.
  * 
- * @since Thematic 2.0.0
  */
-if ( version_compare( $GLOBALS['wp_version'], '3.6-alpha', '<' ) ) {
-	require get_template_directory() . '/library/extensions/back-compat.php';
+if ( version_compare( $GLOBALS['wp_version'], '4.5', '<' ) ) {
+	require get_template_directory() . '/library/extensions/backwards-compatibility.php';
 }
 
 
 /**
- * Registers action hook: thematic_init 
+ * Registers action hook: deciduous_init 
  * 
- * @since 1.0.0
+ * @since 1.0
  */
-function thematic_init() {
-	do_action('thematic_init');
+function deciduous_init() {
+	do_action('deciduous_init');
 }
 
-
+if ( ! function_exists( 'deciduous_theme_setup' ) ) :
 /**
- * thematic_theme_setup & childtheme_override_theme_setup
+ * deciduous_theme_setup
  *
- * Override: childtheme_override_theme_setup
- *
- * @since 1.0.0
  */
-if ( function_exists('childtheme_override_theme_setup') ) {
-	/**
-	 * @ignore
-	 */
-	function thematic_theme_setup() {
-		childtheme_override_theme_setup();
-	}
-} else {
-	/**
-	 * thematic_theme_setup
-	 *
-	 * @todo review for impact of deprecations on child themes & fix comment blocks?
-	 * @since 1.0.0?
-	 */
-	function thematic_theme_setup() {
-		global $content_width;
+function deciduous_p_theme_setup() {
+    global $content_width;
 
-		/**
-		 * Set the content width based on the theme's design and stylesheet.
-		 *
-		 * Used to set the width of images and content. Should be equal to the width the theme
-		 * is designed for, generally via the style.css stylesheet.
-		 *
-		 * @since 1.0.0
-		 */
-		if ( !isset($content_width) ) {
-			$content_width = 600;
-		}
-		
-		// Check for MultiSite
-		define( 'THEMATIC_MB', is_multisite()  );
-
-		// Create the feedlinks
-		if ( ! current_theme_supports( 'thematic_legacy_feedlinks' ) ) {
-			add_theme_support( 'automatic-feed-links' );
-		}
- 
-		if ( apply_filters( 'thematic_post_thumbs', true ) ) {
-			add_theme_support( 'post-thumbnails' );
-		}
- 
-		add_theme_support( 'thematic_superfish' );
-		
-		// Path constants
-		define( 'THEMATIC_LIB',  get_template_directory() .  '/library' );
-
-		// Create Theme Options Page
-		require_once ( THEMATIC_LIB . '/extensions/theme-options.php' );
-		
-		// Need a little help from our helper functions
-		require_once ( THEMATIC_LIB . '/extensions/helpers.php' );
-		
-		// Load legacy functions
-		require_once ( THEMATIC_LIB . '/legacy/deprecated.php' );
-		
-		// Load overrides to activate the old xhtml markup if set
-		if ( !is_admin() && thematic_is_legacy_xhtml() ) {
-			require_once ( THEMATIC_LIB . '/legacy/legacy.php' );
-		}
-		
-		// Add functionality only when not using old xhtml markup
-		if( !thematic_is_legacy_xhtml() ) {
-			add_theme_support( 'thematic_meta_viewport' );
-			add_theme_support('thematic_customizer_layout');
-		}
-
-		// Load widgets
-		require_once ( THEMATIC_LIB . '/extensions/widgets.php' );
-
-		// Load custom header extensions
-		require_once ( THEMATIC_LIB . '/extensions/header-extensions.php' );
-
-		// Load custom content filters
-		require_once ( THEMATIC_LIB . '/extensions/content-extensions.php' );
-
-		// Load custom Comments filters
-		require_once ( THEMATIC_LIB . '/extensions/comments-extensions.php' );
-
-		// Load custom discussion filters
-		require_once ( THEMATIC_LIB . '/extensions/discussion-extensions.php' );
-
-		// Load custom Widgets
-		require_once ( THEMATIC_LIB . '/extensions/widgets-extensions.php' );
-
-		// Load the Comments Template functions and callbacks
-		require_once ( THEMATIC_LIB . '/extensions/discussion.php' );
-
-		// Load custom sidebar hooks
-		require_once ( THEMATIC_LIB . '/extensions/sidebar-extensions.php' );
-
-		// Load custom footer hooks
-		require_once ( THEMATIC_LIB . '/extensions/footer-extensions.php' );
-
-		// Add Dynamic Contextual Semantic Classes
-		require_once ( THEMATIC_LIB . '/extensions/dynamic-classes.php' );
-
-		// Load shortcodes
-		require_once ( THEMATIC_LIB . '/extensions/shortcodes.php' );
-
-		// Load Theme Customizer support
-		require_once ( THEMATIC_LIB . '/extensions/customizer.php' );
-		
-		// Adds filters for the description/meta content in archive templates
-		add_filter( 'archive_meta', 'wptexturize' );
-		add_filter( 'archive_meta', 'convert_smilies' );
-		add_filter( 'archive_meta', 'convert_chars' );
-		add_filter( 'archive_meta', 'wpautop' );
-
-		// Remove the WordPress Generator - via http://blog.ftwr.co.uk/archives/2007/10/06/improving-the-wordpress-generator/
-		function thematic_remove_generators() {
- 			return '';
- 		}
- 		if ( apply_filters( 'thematic_hide_generators', true ) ) {
- 			add_filter( 'the_generator', 'thematic_remove_generators' );
-		}
- 
-		// Translate, if applicable
-		load_theme_textdomain( 'thematic', THEMATIC_LIB . '/languages' );
-
-		$locale = get_locale();
-		$locale_file = THEMATIC_LIB . "/languages/$locale.php";
-		if ( is_readable($locale_file) ) {
-			require_once ($locale_file);
-		}
-	}
-}
-
-add_action('after_setup_theme', 'thematic_theme_setup', 10);
-
-
-/**
- * Registers action hook: thematic_child_init
- * 
- * @since 1.0.0
- */
-function thematic_child_init() {
-	do_action('thematic_child_init');
-}
-
-add_action('after_setup_theme', 'thematic_child_init', 20);
-
-
-if ( function_exists('childtheme_override_init_navmenu') )  {
-	/**
-	 * @ignore
-	 */
-	 function thematic_init_navmenu() {
-    	childtheme_override_init_navmenu();
+    /**
+     * Set the content width based on the theme's design and stylesheet.
+     *
+     * Used to set the width of images and content. Should be equal to the width the theme
+     * is designed for, generally via the style.css stylesheet.
+     *
+     * @since 1.0
+     */
+    if ( !isset($content_width) ) {
+    	$content_width = 600;
     }
-} else {
-	/**
-	 * Register primary nav menu
-	 * 
-	 * Override: childtheme_override_init_navmenu
-	 * Filter: thematic_primary_menu_id
-	 * Filter: thematic_primary_menu_name
-	 */
-    function thematic_init_navmenu() {
-		register_nav_menu( apply_filters('thematic_primary_menu_id', 'primary-menu'), apply_filters('thematic_primary_menu_name', __( 'Primary Menu', 'thematic' ) ) );
-	}
+    
+    // WordPress Core Theme Support
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'html5', array(
+    	'comment-list', 
+    	'comment-form',
+    	'search-form',
+    	'gallery',
+    	'caption'
+    ) );
+	add_theme_support( 'custom-header', apply_filters( 'deciduous_f_custom_header_args', array(
+		'width'                  => 960,
+		'height'                 => 240,
+		'flex-height'            => true,
+		'wp-head-callback'       => 'deciduous_custom_header_style', // in library/extensions/header-extensions.php
+	) ) );
+
+    // Deciduous Theme Specific Support
+    add_theme_support( 'deciduous_s_author_info' );
+    add_theme_support( 'deciduous_s_superfish' );
+    add_theme_support( 'deciduous_s_customizer_layout' );
+    add_theme_support( 'deciduous_s_nav_after_content' );
+    	
+    // Path constants
+    define( 'DECIDUOUS_LIB',  get_template_directory() .  '/library' );
+    
+    // Load Action Hooks
+    require_once ( DECIDUOUS_LIB . '/extensions/actions.php' );
+    
+    // Need a little help from our helper functions
+    require_once ( DECIDUOUS_LIB . '/extensions/helpers.php' );
+    
+    // Load deprecated functions
+    require_once ( DECIDUOUS_LIB . '/legacy/deprecated.php' );
+
+    // Load custom header extensions
+    require_once ( DECIDUOUS_LIB . '/extensions/header-extensions.php' );
+
+    require_once ( DECIDUOUS_LIB . '/extensions/post-header-extensions.php' );
+     // Load custom content filters
+    require_once ( DECIDUOUS_LIB . '/extensions/post-content-extensions.php' );
+    
+    require_once ( DECIDUOUS_LIB . '/extensions/post-footer-extensions.php' );
+
+    // Load custom Comments filters
+    require_once ( DECIDUOUS_LIB . '/extensions/post-comments-extensions.php' );
+
+    // Register Sidebars and load Widget Areas on action hooks
+    require_once ( DECIDUOUS_LIB . '/extensions/widget-area-extensions.php' );
+
+    // Add Dynamic Contextual Semantic Classes
+    require_once ( DECIDUOUS_LIB . '/extensions/dynamic-classes.php' );
+
+    // Load Theme Customizer support
+    require_once ( DECIDUOUS_LIB . '/extensions/customizer.php' );
+    
+    // Adds filters for the description/meta content in archive templates
+    add_filter( 'archive_meta', 'wptexturize' );
+    add_filter( 'archive_meta', 'convert_smilies' );
+    add_filter( 'archive_meta', 'convert_chars' );
+    add_filter( 'archive_meta', 'wpautop' );
+
+
+    // Load the textdomain
+    load_theme_textdomain( 'deciduous', DECIDUOUS_LIB . '/languages' );
+
+    $locale = get_locale();
+    $locale_file = DECIDUOUS_LIB . "/languages/$locale.php";
+    if ( is_readable($locale_file) ) {
+    	require_once ($locale_file);
+    }
 }
-add_action('init', 'thematic_init_navmenu');
+
+endif;
+
+add_action('after_setup_theme', 'deciduous_p_theme_setup', 10);
+
+
+
+
+/**
+ * Registers action hook: deciduous_child_init
+ * 
+ */
+function deciduous_do_childtheme_init() {
+	do_action('deciduous_a_childtheme_init');
+}
+
+add_action('after_setup_theme', 'deciduous_do_childtheme_init', 20);
+
+
+if ( ! function_exists( 'deciduous_p_register_navmenu' ) ) :
+/**
+ * Register primary nav menu
+ * 
+ * Filter: deciduous_primary_menu_id
+ * Filter: deciduous_primary_menu_name
+ */
+
+function deciduous_p_register_navmenu() {
+	register_nav_menu( apply_filters( 'deciduous_f_primary_menu_id', 'primary-menu' ), apply_filters( 'deciduous_f_primary_menu_name', esc_html__( 'Primary Menu', 'deciduous' ) ) );
+}
+
+endif;
+
+add_action( 'init', 'deciduous_p_register_navmenu' );
+
