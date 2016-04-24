3b75e44bc3e5828e147d14d956410d9f0754deb4
emhr <eugene.marseilles.hornot.robinson@gmail.com>
2016-04-24 16:52:43 -0400

    Returning header-extenstions.php and footer-extensions.php code into header.php and footer.php
    Again with the trend of putting power back in to the templates to
    increase legibility.
    
    The contents Thematic's head tag was completely abstracted to
    header-extenstions.php. Over time it got messy with added functions for
    backwards compatibility as Thematic complied theme review requirements.
    And Thematic's migration to an html5 doctype added  filterable markup
    in header.php which I see as unnecessary now.
    
    I am putting the contents some of those functions back into the
    header.php. This puts references to filters and action directly into
    the template. So no more reading though a lengthy file to get to the
    complete picture of what is happening or what is possible.
    
    The branding and navigation #access are being moved to template-parts.
    This will hopefully replace the flexibility/overridability that was
    lost with the removal of the action hook thematic_header. Thematic used
    the priority of functions hooked to that action to build the header
    element.
    
    The footer.php is losing it's filterable markup. It's sidebars in the
    subsidiaries are now called in the proper way to allow the get_sidebar
    hook to affect them. And finally a wrapping div id="colophon" was added
    to contain the div id="siteinfo". I wanted some action hooks and
    flexibility for styling and adding content around the footer text in
    it's own div.

--- footer.php
+++ footer.php
@@ -4,45 +4,79 @@
  *
  * This template closes #main div and displays the #footer div.
  *
- * Thematic Action Hooks: thematic_abovefooter thematic_belowfooter thematic_after
- * Thematic Filters: thematic_close_wrapper can be used to remove the closing of the #wrapper div
- *
- * @package Thematic
+ * @package Deciduous
  * @subpackage Templates
  */
+?>
+  		<?php
+  			// action hook for placing content above the closing of the #main div
+			deciduous_do_before_main_close();
+		?>
+		</div><!-- #main -->
+		
+		<?php
+			// action hook for placing content above the footer
+			deciduous_do_before_footer(); 
+		?>
+		
+		<footer id="footer" class="site-footer">
+			
+			<?php    
+				// action hook for placing content before the subsidiary widget areas
+    			deciduous_do_before_sub_asides();
+    
+     			// Only output Subsidiary markup if one of the asides has a widget in it
+    			if ( is_active_sidebar( '1st-subsidiary-aside' ) || is_active_sidebar( '2nd-subsidiary-aside' ) || is_active_sidebar( '3rd-subsidiary-aside' ) ) : 
+    		?>
+        	
+        	<div id="subsidiary">
+    			<?php
+					deciduous_get_sidebar( '1st-subsidiary' );
+					deciduous_get_sidebar( '2nd-subsidiary' );
+					deciduous_get_sidebar( '3rd-subsidiary' );
+   	 			?>	        
+        	</div><!-- #subsidiary -->
+			
+			<?php 
+				endif;
+				
+				// action hook for placing content after subsidiary widget areas
+				deciduous_do_after_sub_asides();
+			?>
+			<div id="colophon" role="contentinfo"> 
+    			
+    			<?php 
+    				deciduous_do_before_siteinfo();
+    				if ( $deciduous_footer_txt = deciduous_get_theme_opt( 'footer_txt' ) ) : 
+    			?>
+    			
+    			<div id="siteinfo">
+					<p><?php echo( do_shortcode( $deciduous_footer_txt ) )?></p>
+				</div><!-- #siteinfo -->
+    			
+    			<?php  
+    				endif;
+    				deciduous_do_after_siteinfo();
+    			?>
+    			
+			</div><!-- #colophon -->
 
-  	// action hook for placing content above the closing of the #main div
-	thematic_abovemainclose();
+		</footer><!-- .site-footer -->
+		
+		<?php
+			// action hook for placing content below the footer
+			deciduous_do_after_footer();
+		?>
+		
+	</div><!-- #wrapper .hfeed -->
+	
+	<?php 
+		// action hook for placing content before closing the BODY tag
+		deciduous_do_after_wrapper();
 
-	// Filter provided for altering output of the #main div closing element
-	echo ( apply_filters( 'thematic_close_main', '</div><!-- #main -->' . "\n" ) );
-
-	// action hook for placing content above the footer
-	thematic_abovefooter();
-
-	// Filter provided for altering output of the footer opening element
-	echo ( apply_filters( 'thematic_open_footer', '<footer id="footer" class="site-footer">' ) );
-
-	// action hook creating the footer
-	thematic_footer();
-
-	// Filter provided for altering output of the footer closing element
-	echo ( apply_filters( 'thematic_close_footer', '</footer><!-- .site-footer -->' . "\n" ) );
-
-	// action hook for placing content below the footer
-	thematic_belowfooter();
-
-	// Filter provided for altering output of wrapping element follows the body tag
-	echo ( apply_filters( 'thematic_close_wrapper', '</div><!-- #wrapper .hfeed -->' . "\n" ) );
-
-	// action hook for placing content before closing the BODY tag
-	thematic_after();
-
-	// calling WordPress' footer action hook
-	wp_footer();
-
-	// Filter provided for altering output of the body closing tag
-	echo ( apply_filters( 'thematic_close_body', '</body>' . "\n" ) );
-
-	// Filter provided for altering output of the html closing tag
-	echo ( apply_filters( 'thematic_close_html', '</html>' ) );
+		// calling WordPress' footer action hook
+		wp_footer();
+	?>
+	
+</body>
+</html>
